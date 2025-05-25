57eb89ac6e1d37d9a78a95642a1eb0714cd3a68a
Pubudu Premasiri <100259811+Pubudu-98@users.noreply.github.com>
2025-05-25 22:34:14 +0800

    basic python operations

---
+++ basic_operations.py
@@ -0,0 +1,18 @@
+# Mathematical operations in Python
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
+
+# String operations in Python
+greeting = 'Hello'
+name = 'Pubudu'
+print(greeting + ' ' + name)
+
+# Taking user inputs
+user_name = input('Enter Your Name: ')
+print('Hello ' + user_name)
