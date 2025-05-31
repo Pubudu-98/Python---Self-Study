8473a8da6e65d8e86410c836d6937a5ebd6aae3b
Pubudu Premasiri <100259811+Pubudu-98@users.noreply.github.com>
2025-05-31 13:19:53 +0800

    Exersise completed

--- basic_operations.py
+++ basic_operations.py
@@ -16,3 +16,13 @@ print(greeting + ' ' + name)
 # Taking user inputs
 user_name = input('Enter Your Name: ')
 print('Hello ' + user_name + ' How are you?')
+
+# Exersise: Write a program to take two numbers from the user and print their sum, difference, product, and quotient.
+
+input1 = int(input('Enter the first number'))
+input2 = int(input('Enter the second number'))
+
+print('Sum:', input1 + input2)
+print('Difference: ', input1 - input2)
+print('Product:', input1 * input2)
+print('Quotient: ', input1 / input2)
