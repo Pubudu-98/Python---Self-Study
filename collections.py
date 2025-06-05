996c1ea59bc7de7372e907243aa433f0c3e91da0
Pubudu Premasiri <100259811+Pubudu-98@users.noreply.github.com>
2025-06-05 11:20:49 +0800

    Completed exersice.

--- collections.py
+++ collections.py
@@ -2,6 +2,8 @@
 
 # -Lists
 # Lists are mutable, ordered collections of items.
+# They can contain duplicate values and can be modified after creation.
+# Lists are defined using square brackets [].
 fruits = ['apple', 'banana', 'cherry']
 print('This is a list', fruits)
 print(fruits[0]) # accessing the first item
@@ -13,6 +15,7 @@ print('After remove: ', fruits)
 # -Tuples
 # Tuples are immutable, ordered collections of items.
 # They cannot be changed after creation.
+# They can contain duplicate values.
 fruits_tuple = ('apple', 'banana', 'cherry')
 print('This is a tuple: ', fruits_tuple)
 print(fruits_tuple[0])  # accessing the first item
@@ -29,3 +32,10 @@ color.add('red') # Adding a duplicate item has no effect in Sets in python.
 print('After adding a duplicate item', color)
 
 # Exercise
+cars = ['Swift', 'A45S', 'Civic']
+print('This is a list: ', cars)
+cars.append('Corolla')
+print('After appending a new item: ', cars)
+
+cars_tuple = tuple(cars) # Converting a list to a tuple
+print('This is a tuple: ', cars_tuple)
