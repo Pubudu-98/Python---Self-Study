756d9bb6b123d848670da1f084a0a550b0555317
Pubudu Premasiri <100259811+Pubudu-98@users.noreply.github.com>
2025-06-08 22:07:13 +0800

    Functions in python.

--- functions.py
+++ functions.py
@@ -15,4 +15,15 @@ print(message1)
 message2 = greet('Alice')
 print(message2)
 
+# -Exercise
+def add(a, b):
+    return print (a + b)
+result = add(3, 5)
+print(result)
+
+#-Exercise to calculate area
+def calculate_area(length, width):
+    return length * width
+area = calculate_area(10, 20)
+print('Area of the sqaure: ', area)
     
