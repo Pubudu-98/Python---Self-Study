c056c0e87e7e378d4b5b539706778afadcc268c8
Pubudu Premasiri <100259811+Pubudu-98@users.noreply.github.com>
2025-06-08 11:48:48 +0800

    Functions in python

---
+++ functions.py
@@ -0,0 +1,18 @@
+# --Functions in Pythion--
+# Functions are defined using the `def` keyword.
+# Functions can take parameters and return values.
+# Functions can be called by their name followed by parentheses.
+
+def function_name (parameter):
+    # function body
+    return parameter
+
+def greet(name):
+    return print('Hello,', name)
+
+message1 = greet('Pubudu')
+print(message1)
+message2 = greet('Alice')
+print(message2)
+
+    
