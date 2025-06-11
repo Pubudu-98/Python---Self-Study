91c5b451fa3778047f61e98e15956d439921ba8f
Pubudu Premasiri <100259811+Pubudu-98@users.noreply.github.com>
2025-06-11 14:23:26 +0800

    Conditional statements in python

--- conditional_statements.py
+++ conditional_statements.py
@@ -3,9 +3,9 @@
 # if condition:
     # This block executes if the condition is True.
 # elif another_condition:
-    # coed block if another condition is true.
+    # this code block if another condition is true.
 # else:
-    # code block if all conditions are false.
+    # if all conditions are false.
 
 age = int(input('Enter your age: '))
 
